92588ca6a712eb2b3640776a0d5b86a6501e6e52
Jimena Gallegos <jimenagallegosr@outlook.com>
2025-10-22 19:41:43 -0600

    arreglar erorres jiji

--- Avance1.py
+++ Avance1.py
@@ -43,21 +43,23 @@ tokens = [
 
 #Regex for simple tokens
 
-t_equal = r'\='
-t_plus = r'\+'
-t_minus = r'-'
-t_times = r'\*'
-t_divide = r'/'
-t_greater = r'<'
-t_less = r'>'
-t_different = r'!='
-t_rparentesis = r'\)'
-t_lparentesis = r'\()'
-t_rbrackets = r'\]'
-t_lbrackets = r'\[]'
-t_colon = r':'
-t_semicolon = r';'
-t_comma = r','
+t_EQUALS = r'\='
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
+t_GREATER = r'>'
+t_LESS = r'<'
+t_DIFFERENT = r'!='
+t_RPARENTESIS = r'\)'
+t_LPARENTESIS = r'\('
+t_RBRACKETS = r'\]'
+t_LBRACKETS = r'\['
+t_RBRACES = r'\}'
+t_LBRACES = r'\{'
+t_COLON = r':'
+t_SEMICOLON = r';'
+t_COMMA = r','
 
 # Regex rule with some action code
 
@@ -66,17 +68,33 @@ def t_ID(t):
     t.type = reserved.get(t.value, 'ID')
     return t
 
-def t_INT(t):
-    r'\d+'
-    t.value = int(t.value)
-    return t
-
-def t_FLOAT(t):
-    r'\d+\.\d+'
+def t_CTE_FLOAT(t):
+    r'[0-9]+\.[0-9]+'
     t.value = float(t.value)
     return t
 
-def t_STRING(t):
-    r'\"\.*?\"'
+def t_CTE_INT(t):
+    r'[0-9]+'
+    t.value = int(t.value)
+    return t
+
+def t_CTE_STRING(t):
+    r'\".*?\"'
     t.value = t.value[1:-1]
     return t
+
+t_ignore = ' \t\r'
+
+def t_comment(t):
+    r'\/\/.*'
+    pass
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+def t_error(t):
+    print(f"Carácter ilegal '{t.value[0]}' en línea {t.lexer.lineno}")
+    t.lexer.skip(1)
+
+lexer = lex.lex()
