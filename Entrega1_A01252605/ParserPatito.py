f47a5473db5a5b126b2e8b076344e01079e396b7
Jimena Gallegos <jimenagallegosr@outlook.com>
2025-11-06 22:33:50 -0600

    cambio de nombre

--- Entrega1_A01252605/ParserPatito.py
+++ Entrega1_A01252605/ParserPatito.py
@@ -1,25 +1,41 @@
 import ply.yacc as yacc
 from LexerPatito import tokens
+import semantic as semantic
 
 
 def p_program(p):
-    'program : PROGRAM ID SEMICOLON declaraciones funciones MAIN body END'
+    'program : PROGRAM create_dirfunc ID create_id SEMICOLON declaraciones funciones MAIN body END clean_program'
     print("Programa válido")
 
-def p_declaraciones_vars(p):
-    'declaraciones : vars'
+def p_clean_program(p):
+    'clean_program :'
+    print("Paso 6 (Final), Eliminando DirFunc y tabla global")
+
+    # comentados por test
+    #semantic2.func_dir = None
+    #semantic2.current_function = None
+
+def p_create_dirfunc(p):
+    'create_dirfunc :'
+    semantic.func_dir = semantic.FunctionDirectory()
+    semantic.current_function = None
+    print("Paso 1, Directorio de funciones creado")
+
+def p_create_id(p):
+    'create_id :'
+    program_name = p[-1]
+    semantic.func_dir.add_function(program_name, 'program')
+    semantic.current_function = program_name
+    print(f"Paso 2, Nombre de programa registrado: '{program_name}'")
+
+def p_declaraciones(p):
+    '''declaraciones : vars
+                     | empty '''
     pass
 
-def p_declaraciones_vacias(p):
-    'declaraciones : '
-    pass
-
-def p_funciones_recursivo(p):
-    'funciones : funcs funciones'
-    pass
-
-def p_funciones_vacias(p):
-    'funciones : '
+def p_funciones(p):
+    '''funciones : funcs funciones
+                 | empty'''
     pass
 
 def p_vars(p):
@@ -28,265 +44,249 @@ def p_vars(p):
 
 def p_declaracion_var(p):
     'declaracion_var : lista_identificadores COLON type SEMICOLON'
-    print(f"Declaración de variable(s): {p[1]}")
+    tipo = semantic.current_type
+    lista = p[1]
+
+    for ident in lista:
+        semantic.func_dir.add_var(semantic.current_function, ident, tipo)
+        print(f"Paso 5, Variable agregada: {ident} ({tipo}) en {semantic.current_function} Var Table")
     pass
 
-def p_lista_identificadores_mult(p):
-    'lista_identificadores : ID COMMA lista_identificadores'
-    p[0] = [p[1]] + p[3]
+def p_lista_identificadores(p):
+    'lista_identificadores : ID lista_identificadores_prima'
+    if p[2] is None:
+        p[0] = [p[1]]
+    else:
+        p[0] = [p[1]] + p[2]
+    
+    print(f"Paso 3, Variable detectada '{p[0]}'")
+
     pass
 
-def p_lista_identificadores_ind(p):
-    'lista_identificadores : ID'
-    p[0] = [p[1]]
+def p_lista_identificadores_prima(p):
+    '''lista_identificadores_prima : COMMA ID lista_identificadores_prima
+                                    | empty'''
+    if len(p) == 4:
+        if p[3] is None:
+            p[0] = [p[2]]
+        else:
+            p[0] = [p[2]] + p[3]
+
+    else: 
+        p[0] = None
     pass
 
 def p_lista_declaraciones(p):
-    'lista_declaraciones : declaracion_var lista_declaraciones'
+    '''lista_declaraciones : declaracion_var lista_declaraciones
+                           | empty'''
     pass
 
-def p_lista_declaraciones_vacia(p):
-    'lista_declaraciones : '
-    pass
+def p_type(p):
+    '''type : INT_TYPE
+            | FLOAT_TYPE'''
+    semantic.current_type = p[1]
+    p[0] = p[1]
 
-def p_type_int(p):
-    'type : INT_TYPE'
-    print("Tipo detectado: INT")
-    pass
-
-def p_type_float(p):
-    'type : FLOAT_TYPE'
-    print("Tipo detectado: FLOAT")
+    print(f"Paso 4/11, Tipo de variable(s) '{p[0]}'")
     pass
 
 def p_body(p):
     'body : LBRACES lista_statements RBRACES'
-    print("body detectado")
     pass
 
-def p_lista_statements_vacio(p):
-    'lista_statements : '
+def p_lista_statements(p):
+    '''lista_statements : statement lista_statements
+                        | empty'''
     pass
 
-def p_lista_statements_statements(p):
-    'lista_statements : statement lista_statements'
+def p_statement(p):
+    '''statement : print_func
+                 | condition
+                 | cycle
+                 | ID opcion_id
+                 | LBRACKETS lista_statements RBRACKETS'''
     pass
 
-def p_statement_printfunc(p):
-    'statement : print_func'
-    print("Statement: PRINT")
+def p_opcion_id(p):
+    '''opcion_id : f_call SEMICOLON
+                 | assign'''
     pass
 
-def p_statement_condition(p):
-    'statement : condition'
-    pass
-
-def p_statement_cycle(p):
-    'statement : cycle'
-    pass
-
-def p_statement_opcion(p):
-    'statement : ID opcion_id'
-    print(f"Statement con ID: {p[1]}")
-    pass
-
-def p_opcionid_fcall(p):
-    'opcion_id : f_call'
-    pass
-
-def p_opcionid_assign(p):
-    'opcion_id : assign'
-    pass
-
-def p_print_func_placeholder(p):
+def p_print_func(p):
     'print_func : PRINT LPARENTESIS elemento_impresion lista_elementos RPARENTESIS SEMICOLON'
     pass
 
-def p_elemento_impresion_exp(p):
-    'elemento_impresion : expresion'
+def p_elemento_impresion(p):
+    '''elemento_impresion : expresion
+                          | CTE_STRING'''
     pass
 
-def p_elemento_impresion_string(p):
-    'elemento_impresion : CTE_STRING'
-    pass
-
-def p_lista_elementos_recursiva(p):
-    'lista_elementos : COMMA elemento_impresion lista_elementos'
-    pass
-
-def p_lista_elementos_vacia(p):
-    'lista_elementos : '
+def p_lista_elementos(p):
+    '''lista_elementos : COMMA elemento_impresion lista_elementos
+                       | empty'''
     pass
 
 def p_assign(p):
     'assign : EQUALS expresion SEMICOLON'
-    print("Asignación detectada")
     pass
 
 def p_cycle(p):
     'cycle : WHILE LPARENTESIS expresion RPARENTESIS DO body SEMICOLON'
-    print("WHILE detectado")
     pass
 
 def p_condition(p):
-    'condition : IF LPARENTESIS expresion RPARENTESIS body part_else'
+    'condition : IF LPARENTESIS expresion RPARENTESIS body part_else SEMICOLON'
     pass
 
-def p_part_else_body(p):
-    'part_else : ELSE body'
-    print("IF & ELSE detectado")
-    pass
-
-def p_part_else_vacia(p):
-    'part_else : '
-    print("IF detectado")
+def p_part_else(p):
+    '''part_else : ELSE body
+                 | empty'''
     pass
 
 def p_expresion(p):
     'expresion : exp comparacion'
-    print("Expresión detectada")
     pass
 
-def p_comparacion_mayor(p):
-    'comparacion : GREATER exp'
-    pass
-
-def p_comparacion_menor(p):
-    'comparacion : LESS exp'
-    pass
-
-def p_comparacion_diferente(p):
-    'comparacion : DIFFERENT exp'
-    pass
-
-def p_comparacion_vacia(p):
-    'comparacion : '
+def p_comparacion(p):
+    '''comparacion : GREATER exp
+                   | LESS exp
+                   | DIFFERENT exp
+                   | STRICT_EQUAL exp
+                   | empty'''
     pass
 
 def p_exp(p):
     'exp : termino suma_resta'
     pass
 
-def p_suma_resta_suma(p):
-    'suma_resta : PLUS termino suma_resta'
-    pass
-
-def p_suma_resta_resta(p):
-    'suma_resta : MINUS termino suma_resta'
-    pass
-
-def p_suma_resta_vacia(p):
-    'suma_resta : '
+def p_suma_resta(p):
+    '''suma_resta : PLUS termino suma_resta
+                  | MINUS termino suma_resta
+                  | empty'''
     pass
 
 def p_termino(p):
     'termino : factor mult_div'
     pass
 
-def p_mult_div_mult(p):
-    'mult_div : TIMES factor mult_div'
+def p_mult_div(p):
+    '''mult_div : TIMES factor mult_div
+                | DIVIDE factor mult_div
+                | empty'''
     pass
 
-def p_mult_div_div(p):
-    'mult_div : DIVIDE factor mult_div'
+def p_factor(p):
+    '''factor : agrupacion
+              | signo_unario
+              | cte
+              | ID id_opcion'''
     pass
 
-def p_mult_div_vacio(p):
-    'mult_div : '
-    pass
-
-def p_factor_agrupacion(p):
-    'factor : agrupacion'
-    pass
-
-def p_factor_signo(p):
-    'factor : signo_unario'
-    pass
-
-def p_factor_valor(p):
-    'factor : valor'
+def p_id_opcion(p):
+    '''id_opcion : f_call
+                 | empty'''
     pass
 
 def p_agrupacion(p):
     'agrupacion : LPARENTESIS expresion RPARENTESIS'
     pass
 
-def p_signo_unario_suma(p):
-    'signo_unario : PLUS valor'
+def p_signo_unario(p):
+    '''signo_unario : PLUS valor
+                    | MINUS valor'''
     pass
 
-def p_signo_unario_resta(p):
-    'signo_unario : MINUS valor'
+def p_valor(p):
+    '''valor : ID
+             | cte'''
     pass
 
-def p_valor_id(p):
-    'valor : ID'
-    pass
-
-def p_valor_cte(p):
-    'valor : cte'
-    pass
-
-def p_cte_int(p):
-    'cte : CTE_INT'
-    pass
-
-def p_cte_float(p):
-    'cte : CTE_FLOAT'
+def p_cte(p):
+    '''cte : CTE_INT
+           | CTE_FLOAT'''
     pass
 
 def p_funcs(p):
-    'funcs : VOID ID LPARENTESIS parametros RPARENTESIS LBRACKETS bloque_funcion RBRACKETS SEMICOLON'
-    print(f"Nombre de función detectada: {p[2]}")
-    pass
+    'funcs : prepare_new_func funcs_type add_current_type ID add_function LPARENTESIS start_func_vars parametros RPARENTESIS LBRACES bloque_funcion RBRACES end_func SEMICOLON'
 
-def p_parametros_recursivo(p):
-    'parametros : parametro lista_parametros'
-    pass
+def p_prepare_new_func(p):
+    'prepare_new_func :'
+    semantic.current_type = None
+    semantic.current_function = None
+    print("Paso 7, Preparando para nueva función")
 
-def p_parametros_vacio(p):
-    'parametros : '
+def p_funcs_type(p):
+    '''funcs_type : VOID
+                  | type'''
+    p[0] = p[1]
+
+def p_add_current_type(p):
+    'add_current_type :'
+    semantic.current_type = p[-1]
+    print(f"Paso 8, Tipo de funcion detectada '{p[-1]}")
+
+def p_add_function(p):
+    'add_function :'
+    func_name = p[-1]
+    func_type = semantic.current_type
+
+    print(f"Paso 9, Registrado '{func_name}' con '{func_type}'")
+    semantic.func_dir.add_function(func_name, func_type)
+
+    semantic.current_function = func_name
+
+def p_start_func_vars(p):
+    'start_func_vars :'
+    semantic.func_dir.directory[semantic.current_function]['vars'] = semantic.VarTable()
+    print(f"Paso 10: VarTable creada para función '{semantic.current_function}'")
+
+def p_parametros(p):
+    '''parametros : parametro lista_parametros
+                  | empty'''
+    
     pass
 
 def p_parametro(p):
     'parametro : ID COLON type'
+    param_name = p[1]
+    param_type = p[3]
+
+    semantic.func_dir.add_var(semantic.current_function, param_name, param_type)
+    
+    print(f"Paso 11: Parámetro agregado '{param_name}' tipo '{param_type}' a función '{semantic.current_function}'")
+
     pass
 
-def p_lista_parametros_recursivo(p):
-    'lista_parametros : COMMA parametro lista_parametros'
+def p_lista_parametros(p):
+    '''lista_parametros : COMMA parametro lista_parametros
+                        | empty'''
     pass
 
-def p_lista_parametros_vacia(p):
-    'lista_parametros : '
+def p_bloque_funcion(p):
+    '''bloque_funcion : vars body
+                      | body'''
     pass
 
-def p_bloque_funcion_variables(p):
-    'bloque_funcion : vars body'
-    pass
+def p_end_func(p):
+    'end_func :'
+    print(f"Paso 12: Eliminando VarTable de función '{semantic.current_function}'")
+    #comentados en los test
+    #semantic2.func_dir.directory[semantic2.current_function]['vars'] = None
+    #semantic2.current_function = None
 
-def p_bloque_funcion_body(p):
-    'bloque_funcion : body'
-    pass
 
 def p_f_call(p):
-    'f_call : LPARENTESIS argumentos RPARENTESIS SEMICOLON'
-    print("F_CALL detectado")
+    'f_call : LPARENTESIS argumentos RPARENTESIS'
     pass
 
-def p_argumentos_lista(p):
-    'argumentos : expresion lista_argumentos'
+def p_argumentos(p):
+    '''argumentos : expresion lista_argumentos
+                  | empty'''
     pass
 
-def p_argumentos_vacia(p):
-    'argumentos : '
-    pass
-
-def p_lista_argumentos_lista(p):
-    'lista_argumentos : COMMA expresion lista_argumentos'
-    pass
-
-def p_lista_argumentos_vacia(p):
-    'lista_argumentos : '
+def p_lista_argumentos(p):
+    '''lista_argumentos : COMMA expresion lista_argumentos
+                        | empty'''
     pass
 
 def p_error(p):
@@ -294,5 +294,9 @@ def p_error(p):
         print(f"Error de sintaxis en '{p.value}' (línea {p.lineno})")
     else:
         print("Error de sintaxis al final del archivo")
+
+def p_empty(p):
+    'empty :'
+    pass
 
 parser = yacc.yacc()
